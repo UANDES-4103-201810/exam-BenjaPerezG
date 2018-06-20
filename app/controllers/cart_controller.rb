59b69dc0307df528702878b480d361e3c90f3fa2
Benjamin Perez <baperez@miuandes.cl>
2018-06-20 17:53:28 -0400

    bug fixed

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def add_item
-    session[:cart_item].append(:pizza)
+    #session[:cart_item].append(:pizza)
     redirect_to(cart_path)
   end
   def index
